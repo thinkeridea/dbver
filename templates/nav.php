b19201352c6326187560ce918be11b4e72d625ea
qiyin <qiyin@thinkeridea.com>
2018-04-26 15:53:36 +0800

    优化菜单栏样式

--- templates/nav.php
+++ templates/nav.php
@@ -7,14 +7,14 @@ use lib\Config;
 .nav a{color:#fff;font-size:16px;}
 .nav>li>a:hover{background:#006699 !important;}
 .nav>li.active{background:#006699 !important;}
-.nav .dropdown {float: left; padding-top: 8px;}
 .navbar{border-radius:0}
 .select_box{float: right;padding-top: 8px;}
 .select_box .dropdown{margin-left: 20px;float: left;}
+.nav-container{width:auto;margin:0 auto;}
 </style>
 <script src="<?=STATIC_PATH?>js/jquery1.10.2.js"></script>
 <nav class="navbar" style="background: #148CCA !important;">
-    <div class="container">
+    <div class="container nav-container">
         <div id="navbar" >
             <ul class="nav navbar-nav">
                 <li class="logoText">DB版本控制</li>
@@ -22,22 +22,24 @@ use lib\Config;
                 <li class="<?=$this->navActive=='Sql'?'active':''?>"><a href="<?=U('Sql', 'index');?>">建表SQL</a></li>
                 <li class="<?=$this->navActive=='Version'?'active':''?>"><a href="<?=U('Version', 'index')?>">版本控制</a></li>
                 <li class="<?=$this->navActive=='Log'?'active':''?>"><a href="<?=U('Log', 'index');?>">历史记录</a></li>
-                 <?php if (Config::$config['UPGRADES']): ?>
-                    <div class="dropdown">
-                        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                            升级对比:<?=Config::$config['UPGRADE']['NAME'];?>
-                            <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                            <?php foreach(Config::$config['UPGRADES'] as $key=>$item):?>
-                                <li><a href="<?=U('Upgrade', 'diff', array('up_server'=>$key))?>"><?=$item['NAME']?></a></li>
-                            <?php endforeach; ?>
-                        </ul>
-                    </div>
-                <?php elseif(Config::$config['UPGRADE']): ?>
-                    <li class="<?=$this->navActive=='Upgrade'?'active':''?>"><a href="<?=U('Upgrade', 'diff');?>">升级对比</a></li>
-                <?php endif;?>
             </ul>
+            <div class="select_box">
+            <?php if (Config::$config['UPGRADES']): ?>
+                <div class="dropdown">
+                    <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        升级对比:<?=Config::$config['UPGRADE']['NAME'];?>
+                        <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                        <?php foreach(Config::$config['UPGRADES'] as $key=>$item):?>
+                            <li><a href="<?=U('Upgrade', 'diff', array('up_server'=>$key))?>"><?=$item['NAME']?></a></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php elseif(Config::$config['UPGRADE']): ?>
+                <li class="<?=$this->navActive=='Upgrade'?'active':''?>"><a href="<?=U('Upgrade', 'diff');?>">升级对比</a></li>
+            <?php endif;?>
+            </div>
             <?php if(Config::$config['SERVERS']):?>
             <div class="select_box">
                 <div class="dropdown">
